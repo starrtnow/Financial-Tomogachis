98499a5bc318a248fad283256c12e9ab6322a7a3
Starr Yang <starrtyang@gmail.com>
2017-09-16 03:53:46 -0700

    fucking post works

--- Assets/API.cs
+++ Assets/API.cs
@@ -1,41 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Collections.Specialized;
+using System.Net;
+using UnityEngine.Networking;
+
+public static class Http
+{
+    public static byte[] Post(string uri, NameValueCollection pairs)
+    {
+        byte[] response = null;
+        using (WebClient client = new WebClient())
+        {
+            response = client.UploadValues(uri, pairs);
+        }
+        return response;
+    }
+}
+
 
 public class API : MonoBehaviour {
 
     // Use this for initialization
-    public string URL = "ec2-34-213-177-23.us-west-2.compute.amazonaws.com:8080";
-    public string fuck = "ec2-34-213-177-23.us-west-2.compute.amazonaws.com:8080/transaction?user=1&transname=1&transamt=money&transcat=1";
+    public string URL = "ec2-34-213-177-23.us-west-2.compute.amazonaws.com:8080/transaction?user=1&transname=a&transamt=10&transcat=1";
+    public string fuck = "ec2-34-213-177-23.us-west-2.compute.amazonaws.com:8080/transaction?user=1";
     private bool _lock;
     private WWW _www;
     void Start () {
         _lock = false;
         _www = null;
         MakeTransaction("1", "1", 10, "1");
-        Debug.Log("something ahpeennd");
+        StartCoroutine(fuckmylife());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	}
 
+ 
     void MakeTransaction(string userid, string name, float money, string category)
     {
-        if (!_lock){
-            var form = new WWWForm();
-            form.AddField("user", userid);
-            form.AddField("transname", name);
-            form.AddField("transamt", money.ToString());
-            form.AddField("transcat", category);
-            var www = new WWW(fuck);
-            var result = WaitForRequest(www);
-            Debug.Log(result);
+        var dummy = new WWWForm();
+        UnityWebRequest.Post(URL, dummy);
+
+// Debug.Log(result);
+    }
+
+    IEnumerator fuckmylife()
+    {
+        UnityWebRequest a = UnityWebRequest.Post("ec2-34-213-177-23.us-west-2.compute.amazonaws.com:8080/transaction?user=1&transname=a&transamt=10&transcat=1", new WWWForm());
+        yield return a.Send();
+        if(a.isError)
+        {
+            Debug.Log(a.error);
+        } else
+        {
+            Debug.Log("It bloody worked");
+        }
+
+
+        var dummy = new WWWForm();
+        using (UnityWebRequest www = UnityWebRequest.Post(URL, dummy))
+        {
+            yield return www;
+            print(www);
         }
     }
 
     IEnumerator WaitForRequest(WWW www)
     {
+        Debug.Log("we got here");
         yield return www;
+        print(www.text);
+
     }
 }
